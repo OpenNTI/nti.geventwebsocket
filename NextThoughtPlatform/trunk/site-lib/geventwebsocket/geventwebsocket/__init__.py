21136eaa4dc9e6f1db4c70934777fa90706bc976
jason.madden <jason.madden@8561ef65-ed72-40da-a86f-43558c99bf8f>
2013-03-12 15:32:16 +0000

    More on py3/pypy compat. Mane more things will import and some more will run...anything that directly touches Persistent/ZODB will still fail, as will many specific uses of gevent, but updates are coming for those problems.
    git-svn-id: file:///Users/jmadden/tmp/geventwebsocket@16 8561ef65-ed72-40da-a86f-43558c99bf8f

--- NextThoughtPlatform/trunk/site-lib/geventwebsocket/geventwebsocket/__init__.py
+++ NextThoughtPlatform/trunk/site-lib/geventwebsocket/geventwebsocket/__init__.py
@@ -1,7 +1,2 @@
 version_info = (0, 3, 1)
 __version__ = ".".join(map(str, version_info))
-
-__all__ = ['WebSocketHandler', 'WebSocketError']
-
-from geventwebsocket.handler import WebSocketHandler
-from geventwebsocket.websocket import WebSocketError
