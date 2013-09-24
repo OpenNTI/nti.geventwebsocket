a89d52f9b8164e5c9bec4c9363eafef84e4be87b
Jason Madden <jamadden@gmail.com>
2013-09-24 11:07:41 -0500

    Updating to match packaging practices

---
+++ README.rst
@@ -0,0 +1,10 @@
+=====================
+ nti.geventwebsocket
+=====================
+
+Gevent and gunicorn compatible websockets.
+
+Originally based on code at `https://bitbucket.org/Jeffrey/gevent-websocket`_
+
+Usage
+=====
--- setup.py
+++ setup.py
@@ -1,7 +1,46 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
+import codecs
+
+VERSION = '0.0.1'
+
+entry_points = {
+
+}
 
 setup(
     name = 'nti.geventwebsocket',
-    version = '0.0',
-	packages = ['geventwebsocket', 'geventwebsocket.gunicorn'],
-    )
+    version = VERSION,
+    author = 'Jason Madden',
+    author_email = 'open-source@nextthought.com',
+    description = ('Gevent and gunicorn compatible websockets'),
+    long_description = codecs.open('README.rst', encoding='utf-8').read(),
+    license = 'Apache',
+    keywords = 'nose exceptions zope',
+    url = 'https://github.com/NextThought/nti.geventwebsocket',
+    classifiers = [
+		'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+        ],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	install_requires=[
+		'setuptools',
+		'gevent',
+		'greenlet',
+		'zope.interface'
+	],
+	setup_requires = [
+		# Without this, we don't get data files in sdist,
+		# which in turn means tox can't work
+		'setuptools-git'
+	],
+#	namespace_packages=['nti'],
+	entry_points=entry_points
+)
